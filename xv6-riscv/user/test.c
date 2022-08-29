74753044826eb6d0fb92832bee32e6d9ba5c1be9
Samadeol <samartha20@iitk.ac.in>
2022-08-29 22:06:26 +0530

    getppid implemented, run test in xv6 terminal

--- xv6-riscv/user/test.c
+++ xv6-riscv/user/test.c
@@ -3,9 +3,8 @@
 #include "user/user.h"
 
 int main(){
-    if(fork()==0){
-        printf("Child: My pid = %d, and parent pid = %d\n",getpid(),getppid());
-    }else{
+    if(fork()==0) printf("Child: My pid = %d, and parent pid = %d\n",getpid(),getppid());
+    else{
         wait(0);
         printf("Parent: My pid = %d, and parent pid = %d\n",getpid(),getppid());
     }
